f1a363236281b51110f59b5a9b9f4c9f61258f0b
W. Pandula <52339029+PanduDcau@users.noreply.github.com>
2024-08-07 23:01:22 +0530

    papaw model

---
+++ Agriculture/PapawModel/papaw.py
@@ -0,0 +1,46 @@
+from roboflow import Roboflow
+import cv2
+import uuid
+
+import random
+# Generate a random three-digit number
+random_id = str(random.randint(100, 999))
+
+# Use the random number as the image name prefix
+image_name = f"predictions_{random_id}.jpg"
+
+
+rf = Roboflow(api_key="zpJpqip0iN5TpMWJPaM2")
+project = rf.workspace().project("pepper-segmentation")
+model = project.version(1).model
+
+# infer on a local image
+response = model.predict("images/pp7.JPG", confidence=38, overlap=30).json()
+
+pepper = 0
+papaw = 0
+for pred in response['predictions']:
+    print(pred['class'])
+    if pred['class'] == 'pepper':
+        pepper += 1
+    if pred['class'] == 'papaw':
+        papaw += 1
+print("Number of Pepper Seeds \t" + str(pepper))
+
+print("Number of Papaw Seeds \t" + str(papaw))
+
+# visualize your prediction
+model.predict("images/pp66.JPG", confidence=35, overlap=30).save(f"prediction/{image_name}")
+print(f"prediction/{image_name}")
+
+pred_image = cv2.imread(f'prediction/{image_name}', 1)
+imS = cv2.resize(pred_image, (900, 750))
+cv2.imshow('color image', imS)
+
+# Waits for a keystroke
+cv2.waitKey(0)
+# Destroys all the windows created
+cv2.destroyAllwindows()
+
+# infer on an image hosted elsewhere
+# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
---
+++ Agriculture/PapawModel/papaw2.py
@@ -0,0 +1,11 @@
+# import the inference-sdk
+from inference_sdk import InferenceHTTPClient
+
+# initialize the client
+CLIENT = InferenceHTTPClient(
+    api_url="http://detect.roboflow.com",
+    api_key="zpJpqip0iN5TpMWJPaM2"
+)
+
+# infer on a local image
+result = CLIENT.infer("YOUR_IMAGE.jpg", model_id="pepper-segmentation/1")
